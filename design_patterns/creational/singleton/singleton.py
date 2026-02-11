4fcdc36983142817c42456f8d289c34dfc4c91a5
Anjali Singh <singhanjali33375@gmail.com>
2026-02-11 10:26:07 +0530

    Create singleton.py

---
+++ design_patterns/creational/singleton/singleton.py
@@ -0,0 +1,13 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+
+if __name__ == "__main__":
+    obj1 = Singleton()
+    obj2 = Singleton()
+    print(obj1 == obj2)
